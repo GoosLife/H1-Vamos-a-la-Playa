266fab43cb1130a72745b56b9a91793863ef32b0
GoosLife <72657308+GoosLife@users.noreply.github.com>
2022-08-15 14:07:43 +0200

    Add project files.

---
+++ H1 Vamos a la Playa/Program.cs
@@ -0,0 +1,26 @@
+﻿int emptySpots = 0;
+
+int[,] spots = new int[6, 9]
+{
+    { 1, 1, 1, 1, 0, 1, 1, 1, 1 },
+    { 1, 1, 0, 0, 0, 1, 1, 1, 1 },
+    { 1, 1, 1, 1, 1, 1, 0, 0, 1 },
+    { 1, 0, 0, 1, 1, 1, 1, 1, 1 },
+    { 1, 1, 1, 1, 0, 1, 1, 1, 1 },
+    { 1, 1, 0, 0, 0, 1, 0, 0, 0 }
+};
+
+for (int i = 0; i < 6; i++)
+{
+    for (int j = 1; j < 7; j++)
+    {
+        if (spots[i, j - 1] == 0 && spots[i, j + 1] == 0)
+        {
+            emptySpots++;
+            spots[i, j] = 1;
+        }
+    }
+}
+
+Console.WriteLine(emptySpots);
+Console.ReadKey();
